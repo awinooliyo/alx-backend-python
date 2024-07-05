9b38931dd73e9ac0f58bee2972d5da263b00fce1
awinooliyo <erick.otieno89@gmail.com>
2024-07-05 21:26:20 +0300

    task 0

---
+++ 0-add.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module: add
+
+This module contains a function to add two float numbers.
+
+Functions:
+    add(a: float, b: float) -> float:
+        Adds two float numbers and returns the result as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the result.
+
+    Args:
+        a (float): The first number to be added.
+        b (float): The second number to be added.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
