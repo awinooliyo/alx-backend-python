6829a0caf4df88031a4ad43527ba0846d38771a8
awinooliyo <erick.otieno89@gmail.com>
2024-07-05 22:55:23 +0300

    task 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,21 +5,12 @@ Module: safe_first_element
 Defines a function to safely retrieve the
 first element from a list of unknown types.
 """
+import typing
 
-from typing import Sequence, Union
 
-
-def safe_first_element(lst: Sequence) -> Union[None, object]:
-    """
-    Safely retrieves the first element from the input list if it exists.
-
-    Args:
-        lst (Sequence): Input list of elements of unknown type.
-
-    Returns:
-        Union[None, object]: The first element of the input list, or None if
-        the list is empty.
-    """
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
+        typing.Union[typing.Any, None]:
+    """Duck-typed annotation"""
     if lst:
         return lst[0]
     else:
