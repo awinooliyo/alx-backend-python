0385f54f2d55129461cecd9fa8dc4562d74cae0b
awinooliyo <erick.otieno89@gmail.com>
2024-07-05 21:40:04 +0300

    task 4 solution

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module: define_variables
+
+This module defines and annotates variables with specified values.
+
+Variables:
+    a (int): An integer with a value of 1.
+    pi (float): A float with a value of 3.14.
+    i_understand_annotations (bool): A boolean with a value of True.
+    school (str): A string with a value of "Holberton".
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
