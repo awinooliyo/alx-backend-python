d65f6564ac4dbce7bef1d362cc088bb7edc59779
awinooliyo <erick.otieno89@gmail.com>
2024-07-08 12:55:01 +0300

    pep 8

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,8 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Waits for a random delay between 0 and max_delay seconds and returns the delay.
+    Waits for a random delay between 0 and max_delay seconds
+    and returns the delay.
 
     Args:
         max_delay (int): Maximum delay in seconds (default is 10).
