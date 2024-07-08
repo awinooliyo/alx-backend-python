228127c7e315e181b9af0bf2395065055e37411f
awinooliyo <erick.otieno89@gmail.com>
2024-07-08 13:33:36 +0300

    task 4 solution

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Create asyncio tasks.
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns task_wait_random n times with the specified max_delay
+    and returns a list of all delays.
+
+    Args:
+        n (int): Number of times to spawn task_wait_random.
+        max_delay (int): Maximum delay in seconds.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
