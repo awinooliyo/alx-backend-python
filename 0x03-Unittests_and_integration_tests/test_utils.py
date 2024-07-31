9e80158a4274a7475f8d40ada9b00c6ed6893882
awinooliyo <erick.otieno89@gmail.com>
2024-07-31 07:42:18 +0300

    trying task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -64,14 +64,10 @@ class TestGetJson(unittest.TestCase):
             test_payload (dict): The expected JSON payload
             returned by get_json.
         """
-        with patch('utils.requests.get') as mock_get:
-            mock_response = Mock()
-            mock_response.json.return_value = test_payload
-            mock_get.return_value = mock_response
-            result = get_json(test_url)
-            mock_get.assert_called_once_with(test_url)
-            self.assertEqual(result, test_load)
-
+        attrs = {'json.return_value': test_payload}
+        with patch('requests.get', return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
 if __name__ == "__main__":
     unittest.main()
